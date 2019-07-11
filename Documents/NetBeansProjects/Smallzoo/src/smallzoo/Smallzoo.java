08621284cd600c6e5da64b8805f3b318b74dd5bc
nehajohn1022 <nehajohn1022@gmail.com>
2019-07-11 14:42:02 -0400

    All animals are welcome here....

---
+++ Documents/NetBeansProjects/Smallzoo/src/smallzoo/Smallzoo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smallzoo;
+
+/**
+ *
+ * @author 1992999
+ */
+public class Smallzoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("welcome to the zooo");
+    }
+    
+}
